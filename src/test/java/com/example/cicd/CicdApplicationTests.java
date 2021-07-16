4091fc9f951f66abb70e4ae3fae4133e358a34f6
Sorano Lan <soranolan@example.com>
2021-07-16 15:33:36 +0800

    Update test

--- src/test/java/com/example/cicd/CicdApplicationTests.java
+++ src/test/java/com/example/cicd/CicdApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.cicd;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.cicd.demo.service.ITodoListService;
 
 @SpringBootTest
 class CicdApplicationTests {
-
+	
+	@Autowired
+	private ITodoListService service;
+	
 	@Test
 	void contextLoads() {
+		assertThat(service).isNotNull();
 	}
-
+	
 }
