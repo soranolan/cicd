072a32ab14108d1688accb1f9b6e1e7385c9be00
Sorano Lan <soranolan@example.com>
2021-08-05 11:35:32 +0800

    add property to fix insert and select query.

--- src/main/java/com/example/cicd/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/cicd/demo/service/impl/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.example.cicd.demo.service.impl;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import com.example.cicd.core.model.User;
 import com.example.cicd.core.util.Argon2Utils;
+import com.example.cicd.core.util.DateUtils;
 import com.example.cicd.demo.repository.IUserRepository;
 import com.example.cicd.demo.service.IUserService;
 
@@ -23,15 +26,16 @@ public class UserServiceImpl implements IUserService {
 	public Mono<User> findOneByAccount(String account) {
 		User probe = new User();
 		probe.setUsername(account);
+		probe.setEnabled(true);
 		Example<User> example = Example.of(probe);
 		return repository.findOne(example);
 	}
 	
 	@Override
 	public Mono<User> add(User entity) {
-//		String now = DateUtils.now();
-//		if (Optional.ofNullable(entity.getCreatedAt()).isEmpty()) { entity.setCreatedAt(now); }
-//		if (Optional.ofNullable(entity.getUpdatedAt()).isEmpty()) { entity.setUpdatedAt(now); }
+		String now = DateUtils.now();
+		if (Optional.ofNullable(entity.getCreatedAt()).isEmpty()) { entity.setCreatedAt(now); }
+		if (Optional.ofNullable(entity.getUpdatedAt()).isEmpty()) { entity.setUpdatedAt(now); }
 		entity.setPassword(Argon2Utils.encode(entity.getPassword()));
 		return repository.insert(entity);
 	}
