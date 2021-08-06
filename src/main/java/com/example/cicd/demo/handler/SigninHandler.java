be7c34595d7f1124b8090b658619d41bd3e4f4cf
Sorano Lan <soranolan@example.com>
2021-08-06 16:06:21 +0800

    extract helper class.

--- src/main/java/com/example/cicd/demo/handler/SigninHandler.java
+++ src/main/java/com/example/cicd/demo/handler/SigninHandler.java
@@ -1,6 +1,5 @@
 package com.example.cicd.demo.handler;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
@@ -9,8 +8,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 
 import com.example.cicd.core.handler.BaseHandler;
 import com.example.cicd.core.model.User;
-import com.example.cicd.core.util.Argon2Utils;
-import com.example.cicd.core.util.PasetoUtils;
+import com.example.cicd.demo.helper.ISigninHandlerHelper;
 import com.example.cicd.demo.service.IUserService;
 
 import lombok.extern.log4j.Log4j2;
@@ -22,8 +20,11 @@ public class SigninHandler extends BaseHandler {
 	
 	private IUserService service;
 	
-	public SigninHandler(IUserService service) {
+	private ISigninHandlerHelper helper;
+	
+	public SigninHandler(IUserService service, ISigninHandlerHelper helper) {
 		this.service = service;
+		this.helper = helper;
 	}
 	
 	public Mono<ServerResponse> signIn(ServerRequest request) {
@@ -32,17 +33,7 @@ public class SigninHandler extends BaseHandler {
 		
 		Mono<User> existUser = service.findOneByUsername(username);
 		Mono<User> signinUser = request.bodyToMono(User.class);
-		Mono<Map<String, Object>> valid = signinUser.zipWith(existUser, (signin, exist) -> {
-			boolean isMatch = Argon2Utils.matches(signin.getPassword(), exist.getPassword());
-			Map<String, Object> result = new HashMap<String, Object>();
-			result.put("isMatch", isMatch);
-			
-			Map<String, Object> claims = new HashMap<String, Object>();
-			claims.put("role", exist.getRoles());
-			
-			if (isMatch) { result.put("token", PasetoUtils.compact(username, claims)); }
-			return result;
-		});
+		Mono<Map<String, Object>> valid = signinUser.zipWith(existUser, (signin, exist) -> helper.validateCombinator(username, signin, exist));
 		return okResponse(valid, Map.class);
 	}
 	
---
+++ src/main/java/com/example/cicd/demo/helper/impl/SigninHandlerHelperImpl.java
@@ -0,0 +1,29 @@
+package com.example.cicd.demo.helper.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.example.cicd.core.model.User;
+import com.example.cicd.core.util.Argon2Utils;
+import com.example.cicd.core.util.PasetoUtils;
+import com.example.cicd.demo.helper.ISigninHandlerHelper;
+
+@Component
+public class SigninHandlerHelperImpl implements ISigninHandlerHelper {
+	
+	@Override
+	public Map<String, Object> validateCombinator(String username, User signin, User exist) {
+		boolean isMatch = Argon2Utils.matches(signin.getPassword(), exist.getPassword());
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("isMatch", isMatch);
+		
+		Map<String, Object> claims = new HashMap<String, Object>();
+		claims.put("role", exist.getRoles());
+		
+		if (isMatch) { result.put("token", PasetoUtils.compact(username, claims)); }
+		return result;
+	}
+	
+}
