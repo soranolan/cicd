70f93ea49f74061d6ef375c17104ee78a3e1d884
Sorano Lan <soranolan@example.com>
2021-08-08 15:48:08 +0800

    add filter.

---
+++ src/main/java/com/example/cicd/core/filter/RequestLoggingFilter.java
@@ -0,0 +1,38 @@
+package com.example.cicd.core.filter;
+
+import java.net.InetSocketAddress;
+
+import org.json.JSONObject;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+
+import lombok.extern.log4j.Log4j2;
+import reactor.core.publisher.Mono;
+
+@Log4j2
+@Component
+public class RequestLoggingFilter implements WebFilter {
+	
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+		ServerHttpRequest request = exchange.getRequest();
+		InetSocketAddress remoteAddress = request.getRemoteAddress();
+		JSONObject loggingRequest = new JSONObject();
+		if (remoteAddress != null) {
+			loggingRequest.put("HostAddress", remoteAddress.getAddress().getHostAddress());
+			loggingRequest.put("HostName", remoteAddress.getAddress().getHostName());
+			loggingRequest.put("Port", remoteAddress.getPort());
+		}
+		loggingRequest.put("Method", request.getMethod());
+		loggingRequest.put("URI", request.getURI());
+		loggingRequest.put("Path", request.getPath());
+		loggingRequest.put("Headers", request.getHeaders());
+		loggingRequest.put("QueryParams", request.getQueryParams());
+		log.info("[SEARCH_TAG] loggingRequest >>> [{}]", () -> loggingRequest.toString());
+		return chain.filter(exchange);
+	}
+	
+}
