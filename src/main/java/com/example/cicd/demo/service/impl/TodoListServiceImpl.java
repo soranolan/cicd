f0883ca194cd1989b5ecb9028f9583673275135b
Sorano Lan <soranolan@example.com>
2021-08-15 14:49:56 +0800

    extract base common layer.

--- src/main/java/com/example/cicd/demo/service/impl/TodoListServiceImpl.java
+++ src/main/java/com/example/cicd/demo/service/impl/TodoListServiceImpl.java
@@ -2,15 +2,13 @@ package com.example.cicd.demo.service.impl;
 
 import static com.example.cicd.core.enums.LogStatement.DEFAULT;
 
-import java.util.Optional;
-
 import org.json.JSONObject;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
-import com.example.cicd.core.util.DateUtils;
+import com.example.cicd.core.service.impl.BaseServiceImpl;
 import com.example.cicd.demo.helper.ITodoListServiceHelper;
 import com.example.cicd.demo.model.TodoList;
 import com.example.cicd.demo.repository.ITodoListRepository;
@@ -18,18 +16,18 @@ import com.example.cicd.demo.service.ITodoListService;
 
 import lombok.extern.log4j.Log4j2;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @Log4j2
 @PreAuthorize("hasRole('USER')")
 @Service
-public class TodoListServiceImpl implements ITodoListService {
+public class TodoListServiceImpl extends BaseServiceImpl<TodoList> implements ITodoListService {
 	
 	private ITodoListRepository repository;
 	
 	private ITodoListServiceHelper helper;
 	
 	public TodoListServiceImpl(ITodoListRepository repository, ITodoListServiceHelper helper) {
+		super(repository);
 		this.repository = repository;
 		this.helper = helper;
 	}
@@ -50,49 +48,6 @@ public class TodoListServiceImpl implements ITodoListService {
 		log.info(DEFAULT.value(), () -> logParams);
 		
 		return repository.findAll(example, sort);
-	}
-	
-	@Override
-	public Mono<TodoList> find(String id) {
-		JSONObject logParams = new JSONObject();
-		logParams.put("id", id);
-		log.info(DEFAULT.value(), () -> logParams);
-		
-		return repository.findById(id);
-	}
-	
-	@Override
-	public Mono<TodoList> add(TodoList entity) {
-		String now = DateUtils.now();
-		if (Optional.ofNullable(entity.getCreatedAt()).isEmpty()) { entity.setCreatedAt(now); }
-		if (Optional.ofNullable(entity.getUpdatedAt()).isEmpty()) { entity.setUpdatedAt(now); }
-		
-		JSONObject logParams = new JSONObject();
-		logParams.put("entity", entity);
-		log.info(DEFAULT.value(), () -> logParams);
-		
-		return repository.insert(entity);
-	}
-	
-	@Override
-	public Mono<TodoList> modify(TodoList entity) {
-		String now = DateUtils.now();
-		entity.setUpdatedAt(now);
-		
-		JSONObject logParams = new JSONObject();
-		logParams.put("entity", entity);
-		log.info(DEFAULT.value(), () -> logParams);
-		
-		return repository.save(entity);
-	}
-	
-	@Override
-	public Mono<Void> remove(String id) {
-		JSONObject logParams = new JSONObject();
-		logParams.put("id", id);
-		log.info(DEFAULT.value(), () -> logParams);
-		
-		return repository.deleteById(id);
 	}
 	
 }
